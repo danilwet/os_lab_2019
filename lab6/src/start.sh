7532ce6b04f6639df20b8ce3d3eb0c9ee90a6d87
danilwet <sharlok228@gmail.com>
2021-05-19 10:31:42 +0000

    lab6

--- lab6/src/start.sh
+++ lab6/src/start.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
-#nserv nthreads k mod addres
-
-{
-pkill server
-} &> /dev/null
-
-touch $5
-I=0
-cat /dev/null > $5
-chmod 777 $5
-while [ $I -lt $1 ]
-do
-./server --port $[20001+$I] --tnum $2 &
-echo 127.0.0.1:$[20001+$I] >> $5
-I=$[ $I + 1 ]
-done
-sleep 1
-./client --k $3 --mod $4 --servers $5
+make -f makefile clean
+make
+LD_LIBRARY_PATH=$(pwd)
+./server --port 20001 --tnum 2 &
+./server --port 20002 --tnum 3 &
+./client --k 20 --mod 58 --servers ./servers.txt
